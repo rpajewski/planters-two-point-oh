545a61848fba073200a57a34c44342bbf6c5bae3
Ricky Bobby <richard.pajewski@gmail.com>
2021-07-16 14:54:29 -0500

    started typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,100 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        home: {
+            _id: ID
+            name: String
+            rooms: {
+                _id: ID
+                roomName: String
+                lightLevel: Number
+                plants: {
+                    _id: ID
+                    commonName: String
+                    scientificName: String
+                    image_url: String
+                    description: String
+                    careLevel: Number
+                    toxicity: Boolean
+                    lightLevel: Number
+                    waterReq: Number
+                }
+            }
+        }
+    }
+
+    type Plant {
+        _id: ID
+        commonName: String
+        scientificName: String
+        image_url: String
+        descirption: String
+        careLevel: Number
+        toxicity: Boolean
+        lightLevel: Number
+        waterReq: Number
+    }
+
+    type Home {
+        _id: ID
+        name: String
+        rooms: {
+            _id: ID
+            roomName: String
+            lightLevel: Number
+            plants: {
+                _id: ID
+                commonName: String
+                scientificName: String
+                image_url: String
+                description: String
+                careLevel: Number
+                toxicity: Boolean
+                lightLevel: Number
+                waterReq: Number
+            }
+        }
+    }
+
+    type Room {
+
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    input HomeInput {
+        _id: ID
+        homeName: String
+    }
+
+    input RoomInput {
+
+    }
+
+    input PlantInput {
+        
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        plant(plantId: ID!): Plant
+        plants: [Plant]
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        updateUser(username: String, email: String, password: String, home: HomeInput): User
+        updateHome(name: String, rooms: RoomInput): Home
+    }
+`
+
+module.exports = typeDefs
