8fa6bb333d9f64d930659872286e8d664f54c631
Ricky Bobby <richard.pajewski@gmail.com>
2021-07-21 13:01:22 -0500

    resolver queries done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,16 +5,29 @@ const { signToken } = require('../utils/auth')
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
 
+                return userData
+            }
+            throw new AuthenticationError('Not logged in!')
         },
         users: async () => {
-
+            return await User.find()
+                .select('-__v -password')
         },
         plant: async (parent, args, context) => {
+            if (context.plant) {
+                const plantData = await Plant.findById(args.plantId)
 
+                return plantData
+            }
         },
         plants: async (parent, args, context) => {
-
+            if (context.plants) {
+                return await Plant.find()
+            } 
         }
     },
     Mutation: {
@@ -34,7 +47,7 @@ const resolvers = {
 
         },
         addPlant: async (parent, args) => {
-            
+
         }
     }
 }
